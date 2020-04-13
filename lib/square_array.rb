32c875e173347c98ba24febedb833dc1a7585961
Guosa Edebiri <gedebiri@gmail.com>
2020-04-13 00:03:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
   
   counter = 0 
-  length = array.length
   square_array = []
   
   while array[counter] do
-    square_array.push(array[counter] * array[counter])
+    square_array.push((array[counter]) * (array[counter]))
   end
     
 end
