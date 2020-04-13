076f5b759b2843a164d47ffcfcb9af8e76a14b7e
Guosa Edebiri <gedebiri@gmail.com>
2020-04-13 00:01:50 +0000

    Automatically backed up by Learn

---
+++ lib/square_array.rb
@@ -0,0 +1,11 @@
+def square_array(array)
+  
+  counter = 0 
+  length = array.length
+  square_array = []
+  
+  while array[counter] do
+    square_array.push(array[counter] * array[counter])
+  end
+    
+end
